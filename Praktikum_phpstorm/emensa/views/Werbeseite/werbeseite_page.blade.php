1d7eca2d7f345dca1f278e10f0fdd3376518000e
luca <119941740+acul98@users.noreply.github.com>
2022-12-06 14:46:28 +0100

    test

--- Praktikum_phpstorm/emensa/views/Werbeseite/werbeseite_page.blade.php
+++ Praktikum_phpstorm/emensa/views/Werbeseite/werbeseite_page.blade.php
@@ -46,7 +46,7 @@
             dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
             et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit.</p>
     </div>
 @endsection
 
@@ -61,6 +61,7 @@
 
 
         @foreach($Gerichteausgabe as $value)
+
             <tr>
                 <td> {{$value['bildname']}}</td>
                 <td> {{$value['name']}}</td>
