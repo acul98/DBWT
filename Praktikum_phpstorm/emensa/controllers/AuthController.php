058adc3ab5164f1b93fcfa60c555b0154485821f
luca <119941740+acul98@users.noreply.github.com>
2022-12-10 13:19:55 +0100

    M5 Aufgabe 1Update

--- Praktikum_phpstorm/emensa/controllers/AuthController.php
+++ Praktikum_phpstorm/emensa/controllers/AuthController.php
@@ -29,15 +29,15 @@ class AuthController
                       $_SESSION['login_ok'] = true;
                       $_SESSION['nutzer'] = $e['name']; //ausgabe des Namens auf der Werbeseite, welcher sich erfolgreich angemeldet hat.
 
-                      /*
+
                       $_SESSION['counter']+=1; //der zähler zählt jee erfolgreiche anmeldung eins hoch
                       $counter = $_SESSION['counter'];
                       //Datenbank aufbau und ubdate der anzahlanmeldungen in der Datenbank
                       $link = connectdb();
-                      $anzahlanmeldungensetzten = "UPDATE benutzer SET anzahlanmeldungen='$counter' WHERE name = $email";
+                      $anzahlanmeldungensetzten = "UPDATE benutzer SET anzahlanmeldungen='$counter' WHERE email = '$email'";
                       $resultanzahlanmeldungensetzten = mysqli_query($link, $anzahlanmeldungensetzten);
                       mysqli_close($link);
-                      */
+
 
                       header('Location: /werbeseite'); //zurück auf die Werbeseite
                     }
--- Praktikum_phpstorm/emensa/models/benutzer.php
+++ Praktikum_phpstorm/emensa/models/benutzer.php
@@ -14,29 +14,3 @@ function db_select_benutzer() {
     return $data;
 }
 
-/*
-function db_select_password_user() {
-    $link = connectdb();
-
-    $sql = "SELECT passwort FROM benutzer";
-    $result = mysqli_query($link, $sql);
-
-    $data = mysqli_fetch_all($result, MYSQLI_BOTH);
-
-    mysqli_close($link);
-    return $data;
-}
-
-function db_select_email_user() {
-    $link = connectdb();
-
-    $sql = "SELECT email FROM benutzer";
-    $result = mysqli_query($link, $sql);
-
-    $data = mysqli_fetch_all($result, MYSQLI_BOTH);
-
-    mysqli_close($link);
-    return $data;
-
-}
-*/
