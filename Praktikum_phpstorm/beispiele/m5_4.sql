19fbee06bfe31131a25d23833fad3cdfa8e29555
Elisa <119941438+Elifant3@users.noreply.github.com>
2022-12-10 12:36:39 +0100

    Create m5_4.sql

---
+++ Praktikum_phpstorm/beispiele/m5_4.sql
@@ -0,0 +1,12 @@
+use emensawerbeseite;
+
+CREATE VIEW view_suppengerichte AS
+    SELECT * FROM gericht WHERE name LIKE '%suppe%';
+
+CREATE VIEW view_anmeldungen AS
+    SELECT anzahlanmeldungen, name FROM benutzer ORDER BY anzahlanmeldungen DESC;
+
+CREATE VIEW view_kategoriegerichte_vegetarisch AS
+    SELECT k.name, g.id, g.name AS Gericht FROM kategorie k
+    RIGHT JOIN gericht g ON k.id = g.id
+    WHERE g.vegetarisch = '1';
