3ed55c5138256ccc1bd8cfee527da94cf929880c
luca <119941740+acul98@users.noreply.github.com>
2022-12-13 18:33:42 +0100

    Update m5_5.sql

--- Praktikum_phpstorm/beispiele/m5_5.sql
+++ Praktikum_phpstorm/beispiele/m5_5.sql
@@ -3,3 +3,9 @@ BEGIN
     UPDATE benutzer SET anzahlanmeldungen += 1 WHERE id = id_benutzer;
 end;
 
+
+
+CREATE PROCEDURE anzahlanmedlungen (IN email_db VARCHAR (80))
+BEGIN
+UPDATE benutzer SET anzahlanmeldungen = anzahlanmeldungen+1 WHERE email = email_db;
+end;
