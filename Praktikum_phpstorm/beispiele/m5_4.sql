04af1ca2402d67b6c1b4b92ea2c7b11db3e6b614
Elisa <119941438+Elifant3@users.noreply.github.com>
2022-12-15 08:14:57 +0100

    ka

--- Praktikum_phpstorm/beispiele/m5_4.sql
+++ Praktikum_phpstorm/beispiele/m5_4.sql
@@ -8,7 +8,7 @@ CREATE VIEW view_anmeldungen AS
 
 
 
-CREATE VIEW `view1_kategoriegerichte_vegetarisch` AS
+CREATE VIEW view_kategoriegerichte_vegetarisch AS
     SELECT kategorie.name AS Kategorie, gericht.name AS Vegetarische_Gerichte FROM kategorie
     LEFT JOIN gericht_hat_kategorie ON kategorie.id = gericht_hat_kategorie.kategorie_id                                                                                  left join gericht on gericht_hat_kategorie.gericht_id = gericht.id
     WHERE gericht.vegetarisch = 1 OR gericht_hat_kategorie.kategorie_id IS NULL
