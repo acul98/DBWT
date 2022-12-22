d1f4711490fdbaea613aaf149b78cde88d742c40
luca <119941740+acul98@users.noreply.github.com>
2022-12-22 13:11:33 +0100

    wefew

--- Praktikum_phpstorm/beispiele/m6_a1.sql
+++ Praktikum_phpstorm/beispiele/m6_a1.sql
@@ -1,5 +1,5 @@
 CREATE TABLE bewertungen
-(bewertungs_id INT8 PRIMARY KEY AUTO_INCREMENT,
+(bewertungs_id INT8 PRIMARY KEY,
  bemerkung VARCHAR(200) NOT NULL,
  bewertungszeitpunkt DATETIME,
  hervorgehoben BOOLEAN NOT NULL DEFAULT false,
@@ -7,6 +7,11 @@ CREATE TABLE bewertungen
 );
 
 
+
+
+
+Drop TABLE bewertungen;
+
 ALTER TABLE bewertungen
-   ADD CONSTRAINT b_id_ref_benutzer_id
-      FOREIGN KEY ();
+   ADD CONSTRAINT bewerungen_id_ref_benutzer_id
+      FOREIGN KEY (bewertungs_id) REFERENCES benutzer(id);
