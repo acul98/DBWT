73cb11b5f3506b2ddbd11e0ca7894e8bfb8e6a36
Elisa <119941438+Elifant3@users.noreply.github.com>
2022-12-22 13:24:42 +0100

    m6

--- Praktikum_phpstorm/beispiele/m6_a1.sql
+++ Praktikum_phpstorm/beispiele/m6_a1.sql
@@ -1,12 +1,14 @@
 CREATE TABLE bewertungen
-(bewertungs_id INT8 PRIMARY KEY,
+(bewertungs_id INT8 NOT NULL,
  bemerkung VARCHAR(200) NOT NULL,
  bewertungszeitpunkt DATETIME,
  hervorgehoben BOOLEAN NOT NULL DEFAULT false,
- sternebwertung VARCHAR(80) NOT NULL
+ sternebwertung VARCHAR(80) NOT NULL,
+ gericht_id INT8 NOT NULL
 );
 
-
+CREATE UNIQUE INDEX bewertungen_index
+    ON bewertungen (bewertungs_id, gericht_id);
 
 
 
@@ -15,3 +17,7 @@ Drop TABLE bewertungen;
 ALTER TABLE bewertungen
    ADD CONSTRAINT bewerungen_id_ref_benutzer_id
       FOREIGN KEY (bewertungs_id) REFERENCES benutzer(id);
+
+ALTER TABLE bewertungen
+    ADD CONSTRAINT bewgericht_id_ref_gericht_id
+        FOREIGN KEY (gericht_id) REFERENCES gericht(id);
